30b7a0a14e036b107928e295e98f85150e066eea
Luca De Feo <luca.defeo@polytechnique.edu>
2013-08-11 20:59:47 +0200

    Made script more configurable, added a cleaning option for remacle.org

--- scalpo.sh
+++ scalpo.sh
@@ -1,19 +1,23 @@
 #!/bin/sh
 
-port=8080
-jettyhome=jetty
-wd=$PWD
+host=${HOST:-localhost}
+port=${PORT:-8080}
+jettyhome=${JETTYHOME:-jetty}
+solrhome=${SOLRHOME:-$PWD}
 
 case "$1" in
     jetty)
-	cd $jettyhome && java -Djetty.port=$port -Dsolr.solr.home="$wd/solr" -jar start.jar
+	cd $jettyhome && java -Djetty.port=$port -Dsolr.solr.home="$solrhome/solr" -jar start.jar
 	;;
     crawl)
 	scrapy crawl "$2" -o "$2.json" -t json
 	;;
     index)
-	curl "http://localhost:$port/solr/scalpo/update?wt=json" --data-binary @"$2.json" -H 'Content-type:application/json'
-	curl "http://localhost:$port/solr/scalpo/update?softCommit=true"
+	curl "http://$host:$port/solr/scalpo/update?wt=json" --data-binary @"$2.json" -H 'Content-type:application/json'
+	curl "http://$host:$port/solr/scalpo/update?softCommit=true"
+	;;
+    delete-remacle-indexes)
+	curl "http://$host:$port/solr/scalpo/update?wt=json" --data-binary '{"delete":{"query":"ubuntu AND url:\"remacle.org\""}, "commit":{}}' -H 'Content-type:application/json'
 	;;
     *)
 	echo "Usage: scalpo.sh jetty|index|crawl [source]"
